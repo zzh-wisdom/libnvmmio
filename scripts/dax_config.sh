f0dc712ba492f160e084649e927d1c0338206dd9
zzh-wisdom <1368667276@qq.com>
2023-05-07 18:07:42 +0800

    fix config

--- scripts/dax_config.sh
+++ scripts/dax_config.sh
@@ -6,3 +6,10 @@ sudo mkfs.ext4 -F -b 4096 /dev/pmem0
 sudo mount -o dax /dev/pmem0 /mnt/pmem
 sudo chown -R $USER:$USER /mnt/pmem
 
+echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+sudo rm -rf /mnt/pmem0/*
+sudo umount /mnt/pmem0
+sudo mkfs.ext4 -F -b 4096 /dev/pmem0
+sudo mount -o dax /dev/pmem0 /mnt/pmem0
+sudo chown -R $USER:$USER /mnt/pmem0
+
